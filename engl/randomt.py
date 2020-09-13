8f089f98d8325d9b04d7f6fb65690bb3d7540492
ksakkas <ksakkasuoi@gmail.com>
2020-09-13 20:29:34 +0300

    random in python

---
+++ engl/randomt.py
@@ -0,0 +1,11 @@
+# Production of Random Number
+# CAUTION: do not name the file random.py
+
+# Python does not have a rundom () function to make a random number,
+# but Python has a built-in module called random and can
+# to be used to generate random numbers:
+
+import random  # import random
+
+print(random.randrange(1, 5))  # Generate a random number in the range 1 to 5
+
