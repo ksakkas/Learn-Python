9aa3b982b64376d10b54f5a8765dc6fc6aa5a832
ksakkas <ksakkasuoi@gmail.com>
2020-09-13 19:54:12 +0300

    convert in python

---
+++ engl/convert_num.py
@@ -0,0 +1,15 @@
+# Type Conversion
+
+x = 1
+y = 2.1
+z = 5j
+
+xn = float(x) # int to float
+yn = int(y) # float tp int
+zn = complex(z) # 5j to complex
+
+print(xn)
+print(yn)
+print(zn)
+
+# You cannot convert complex numbers into another number type.
---
+++ greek/convert_num.py
@@ -0,0 +1,18 @@
+# Μετατροπή Αριθμών
+# int = ακέραιος
+# float = δεκαδικός
+# complex = σύνθετος
+
+x = 1
+y = 2.1
+z = 5j
+
+xn = float(x) # int σε float
+yn = int(y) # float σε int
+zn = complex(z) # 5j σε complex
+
+print(xn)
+print(yn)
+print(zn)
+
+# Δεν μπορείτε να μετατρέψετε σύνθετους αριθμούς σε άλλο τύπο αριθμό.
