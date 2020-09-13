564ad3bb243f5cbb9966200c9eb49d110a295140
ksakkas <ksakkasuoi@gmail.com>
2020-09-13 18:25:16 +0300

    python

---
+++ engl/intr_synt.py
@@ -0,0 +1,12 @@
+# To check if you have python installed on a Windows computer,
+# Look for Python in the starting line or do the following
+# at the command prompt (cmd.exe):
+
+# run in cmd: python --version to see if you have downloaded python
+
+# If you find that you do not have python installed on your computer,
+# then you can download it for free from the following site: https://www.python.org/
+
+# After writing a program must be saved (file_name.py)
+
+# To run a program you can run your program via cmd with the python command file_name.py
