89d0c58108d568f90c1ffa90a8e79e02a1e576d9
ksakkas <ksakkasuoi@gmail.com>
2020-09-13 19:37:50 +0300

    data type in python

---
+++ engl/datatype.py
@@ -0,0 +1,11 @@
+# Python has the following data types by default:
+# Text Type: str
+# Numeric Types: int, float, complex
+# Sequence Types: list, tuple, range
+# Mapping Type: dict
+# Set Types: set, frozenset
+# Boolean Type: bool
+# Binary Types: bytes, bytearray, memoryview
+
+x = 5
+print(type(x)) # With the type your program returns the type of the value of the variable
