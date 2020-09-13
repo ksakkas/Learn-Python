ae30099937b52401d1c880e7b986944c554d6e2e
ksakkas <ksakkasuoi@gmail.com>
2020-09-13 18:41:18 +0300

    syntax in python

---
+++ engl/syntax.py
@@ -0,0 +1,12 @@
+# The indentation in Python is very important.
+# Python uses indentation to indicate a block of code.
+
+# Correct code
+if 10 > 8 :
+    print("true")
+
+
+# Wrong code
+if 10 > 8 :
+print("true")
+
