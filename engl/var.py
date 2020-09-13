29a66a444d948d7a904f6de1a6345ef1bebc1a04
ksakkas <ksakkasuoi@gmail.com>
2020-09-13 19:06:10 +0300

    variables in python

---
+++ engl/var.py
@@ -0,0 +1,18 @@
+# Python Variables
+# You do not need a variable type declaration, depending on the value it also gets the type
+
+x = 28
+y = "kostas"
+z = 'k'
+c,a = 5, 3 
+r = t = 0
+
+print(x)
+print(y)
+print(z)
+print(c)
+print(a)
+print(r)
+print(t)
+
+print("Kostas " + z) # ATTENTION: text and variable must be of the same type
