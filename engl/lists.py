f94a4513247113fb9dc7617243ab7234969a58f6
ksakkas <ksakkasuoi@gmail.com>
2020-09-14 21:02:03 +0300

    list in py

---
+++ engl/lists.py
@@ -0,0 +1,34 @@
+# List in Python
+
+ls = ["dit", "uoi", "arta"]
+print(ls)
+
+print("------------------------------------------")
+
+print(ls[0])
+print(ls[1])
+print(ls[2])
+
+print("------------------------------------------")
+
+print(ls[-1])
+
+print("------------------------------------------")
+
+print(ls[0:2])
+
+print("------------------------------------------")
+
+ls.insert(1, "ioannina")    # Insert a item in list
+print(ls)
+
+print("------------------------------------------")
+
+ls.remove("ioannina")   # Remove a item in list
+print(ls)
+
+print("------------------------------------------")
+
+k = ls.copy()   # Copy list
+print(k)
+
