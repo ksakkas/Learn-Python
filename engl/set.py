be47e3dea414d9780d7dca05a8e12e7212793a8c
ksakkas <ksakkasuoi@gmail.com>
2020-09-19 19:03:32 +0300

    set in py

---
+++ engl/set.py
@@ -0,0 +1,9 @@
+# SET
+# A set is a collection which is unordered and unindexed. 
+# In Python, sets are written with curly brackets.
+
+thisset = {"DIT", "UOI", "DEPARTMENT"}
+print(thisset)
+
+thisset.add("arta") # add a item
+print(thisset)
---
+++ greek/set.py
@@ -0,0 +1,9 @@
+# SET
+# Ένα set είναι μια συλλογή που δεν είναι ταξινομημένη και χωρίς κατάταξη. 
+# Στη Python, τα σετ γράφονται με αγκύλες.
+
+thisset = {"DIT", "UOI", "DEPARTMENT"}
+print(thisset)
+
+thisset.add("arta") # προσθήκη ενός στοιχείου
+print(thisset)
