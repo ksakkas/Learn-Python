60cf4bc8791b703b8696ca2c2ee456e98c029982
ksakkas <ksakkasuoi@gmail.com>
2020-09-19 18:57:48 +0300

    tuple in py

---
+++ engl/tuple.py
@@ -0,0 +1,7 @@
+# Tuple
+# A tuple is a collection which is ordered and unchangeable. In Python tuples are written with round brackets.
+
+thistuple = ("dit", "uoi", "department")
+print(thistuple)
+print("")
+print(thistuple[1])
---
+++ greek/tuple.py
@@ -0,0 +1,7 @@
+# Tuple
+# Μια ειδική κατηγορία στην οποία δεν μπορείς να προσθέσεις ή να αφαιρέσεις στοιχεία (μορφή πίνακα)
+
+thistuple = ("dit", "uoi", "department")
+print(thistuple)
+print("")
+print(thistuple[1])
