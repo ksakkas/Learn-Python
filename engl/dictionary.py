0f77f7e70c8296621968edfa6f3a6ca7f2db2269
ksakkas <ksakkasuoi@gmail.com>
2020-09-23 21:51:55 +0300

    dictionary in py

---
+++ engl/dictionary.py
@@ -0,0 +1,15 @@
+# Python Dictionaries
+# A dictionary is a collection which is unordered, changeable and indexed. 
+# In Python dictionaries are written with curly brackets, and they have keys and values.
+# You can access the items of a dictionary by referring to its key name, inside square brackets:
+
+dit = {                                                          # Create a dictionary named dit
+    "departmeny" : "Informatics and Telecommunications",         # Dictionary data
+    "university" : "Ioannina"
+}  
+
+un = dit["university"]                                           # Item search
+print(un)
+
+print(dit)
+
