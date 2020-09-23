e7423fe909f6d7744a99f62cd90694a90e4e04fb
ksakkas <ksakkasuoi@gmail.com>
2020-09-23 22:23:53 +0300

    while in py

---
+++ engl/while.py
@@ -0,0 +1,9 @@
+# While
+# As long as the condition is valid, the execution takes place
+# Python relies on indentation (whitespace at the beginning of a line) to define scope 
+# in the code. Other programming languages often use curly-brackets for this purpose.
+
+i = 1
+while i < 10:
+    print(i)
+    i += 1
