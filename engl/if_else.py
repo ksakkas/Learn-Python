464ce924ec8d2f8104f410475fe78ff1678de0c8
ksakkas <ksakkasuoi@gmail.com>
2020-09-23 22:11:58 +0300

    if else in py

---
+++ engl/if_else.py
@@ -0,0 +1,13 @@
+# IF ELSEIF ELSE
+# Python relies on indentation (whitespace at the beginning of a line) to define scope 
+# in the code. Other programming languages often use curly-brackets for this purpose.
+
+
+a = 5
+b = 10
+if b > a:
+    print("b > a")
+elif a == b:
+    print("a == b")
+else:
+    print("a < b")
