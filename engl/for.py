ace72af52d610ae83e8f07c34d56a8d50fd36cc8
ksakkas <ksakkasuoi@gmail.com>
2020-10-11 12:30:52 +0300

    for in python

---
+++ engl/for.py
@@ -0,0 +1,12 @@
+# Python For Loops
+
+i=0
+for i in range(10):   # i<=10
+    i=i+1             # i=i+1
+    print(i)
+
+print("---------------------------------")
+
+table = ["dit", "uoi", "dit_uoi"]
+for k in table:       # k <= table
+    print(k)
