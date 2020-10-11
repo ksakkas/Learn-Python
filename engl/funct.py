540c7bbdda6a8762169c536919b74f02f3e1fdaa
ksakkas <ksakkasuoi@gmail.com>
2020-10-11 12:39:22 +0300

    function in python

--- engl/funct.py
+++ engl/funct.py
@@ -4,3 +4,8 @@ def mf():             # In Python a function is defined using the def keyword
       print("Hello!!")
 
 mf()                 # Call a function
+
+def mfa(name):     # Function with arguments
+  print(name + " !!!")
+
+mfa("Kostas")
